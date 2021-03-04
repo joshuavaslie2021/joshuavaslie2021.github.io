3cc5e8793ee0f60523e1385e8fc7505d14227cd0
joshuavaslie2021 <joshuavaslie2021@u.northwestern.edu>
2021-03-04 01:25:18 -0600

    components and necessary styling pages added

---
+++ components/Footer.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import "./styles/Footer.css";
+
+
+// const styles = {
+//     footerStyle: {
+//       background: "red"
+//     },
+//     footingStyle: {
+//       fontSize: 100
+//     }
+//   };
+  
+  // We use JSX curly braces to evaluate the style object
+  
+//   function Footer() {
+//     return (
+//       <footer style={styles.footerStyle} className="footer">
+//         <h1 style={styles.footingStyle}>Welcome</h1>
+//       </footer>
+//     );
+//   }
+  
+//   export default Footer;
+  
